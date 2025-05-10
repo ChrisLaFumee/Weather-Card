1c8b80fee882cf309678f00a29270fbb50ec0095
LaFumee <chrislafumee@gmail.com>
2025-05-10 19:45:08 -0400

    new weather updates

--- src/utils/weatherApi.js
+++ src/utils/weatherApi.js
@@ -1,6 +1,6 @@
 export const getWeather = ({ latitude, longitude }, APIkey) => {
   return fetch(
-    "https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&units=imperial&appid=${APIkey}"
+    `https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&units=imperial&appid=${APIkey}`
   ).then((res) => {
     if (res.ok) {
       return res.json();
@@ -13,6 +13,17 @@ export const getWeather = ({ latitude, longitude }, APIkey) => {
 export const filterWeatherData = (data) => {
   const result = {};
   result.city = data.name;
-
+  result.temp = { F: data.main.temp, C: data.main.temp - 32 };
+  result.type = getWeatherType(result.temp.F);
   return result;
+};
+
+const getWeatherType = (temperature) => {
+  if (temperature >= 86) {
+    return "hot";
+  } else if (temperature >= 66 && temperature < 86) {
+    return "warm";
+  } else {
+    return "cold";
+  }
 };
